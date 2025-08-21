8a4ff0f11f9f65df023649e8adafe328628ade6b
Krishanx2003 <learnthefutureai@gmail.com>
2025-08-22 00:03:11 +0530

    ads

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,8 @@
-// "use client";
+"use client";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { Sidebar } from "./_components/Sidebar";
+import Head from "next/head";
 
 export default function DashboardLayout({
   children,
@@ -9,24 +10,31 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <div className="flex h-screen bg-gray-50">
-        {/* Sidebar */}
-        <Sidebar />
+    <>
+      <Head>
+        <meta
+          name="6a97888e-site-verification"
+          content="0dae91cd147e2cc60e2c6f7ec3191362"
+        />
+      </Head>
 
-        {/* Main Content */}
-        <div className="flex-1 flex flex-col overflow-hidden">
-          {/* Page Content */}
-          <main className="flex-1 overflow-auto">
-            {children}
-          </main>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <div className="flex h-screen bg-gray-50">
+          {/* Sidebar */}
+          <Sidebar />
+
+          {/* Main Content */}
+          <div className="flex-1 flex flex-col overflow-hidden">
+            {/* Page Content */}
+            <main className="flex-1 overflow-auto">{children}</main>
+          </div>
         </div>
-      </div>
-    </ThemeProvider>
+      </ThemeProvider>
+    </>
   );
 }
